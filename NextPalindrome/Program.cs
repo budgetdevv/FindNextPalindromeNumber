e4d3638938167fb5e6aa64bdbf7c35289b88b79d
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2023-12-24 15:27:52 +0800

    Implemented ReverseDigitsFast(), will see how well it performs

--- NextPalindrome/Program.cs
+++ NextPalindrome/Program.cs
@@ -1,5 +1,6 @@
 ﻿// #define PRINT_DEBUG
 
+using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -11,20 +12,14 @@ namespace NextPalindrome
         [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         private static void Main(string[] args)
         {
-            //Console.WriteLine(FindHighestPalindrome());
-            
-            // var multipleTableGetDigitsFast = MultiplesTable + 1;
-            //
-            // GetDigitsFast(1073741824, multipleTableGetDigitsFast);
-            //
-            // return;
+            ReverseDigitsFast(100_000, ReversedTwoDigitsTable, 6);
             
             // How to check codegen:
             // Mac:
             // export DOTNET_JitDisasm="Loop"
             // dotnet run -c Release
 
-            // Validate();
+            Validate();
 
             Console.WriteLine("Churning!");
             
@@ -41,6 +36,11 @@ namespace NextPalindrome
             Console.ReadKey();
         }
 
+        private static (uint quotient, uint remainder) DivRemFast(uint num, uint divisor)
+        {
+            return Math.DivRem(num, divisor);
+        }
+        
         private static uint GetHighestPalindrome()
         {
             #if RELEASE
@@ -65,7 +65,7 @@ namespace NextPalindrome
             // Most of the code are copied from GetCurrentOrNextPalindrome(), so jump to that method if you are looking
             // for comments.
             
-            var multiplesTable = MultiplesTable;
+            var multiplesTable = MultiplesOf10TablePtr;
             
             // This should be constant-folded
             var multipleTableGetDigitsFast = multiplesTable + 1;
@@ -150,7 +150,7 @@ namespace NextPalindrome
             {
                 const int ITERATIONS = 1_000_000;
                 
-                var multipleTableGetDigitsFast = MultiplesTable + 1;
+                var multipleTableGetDigitsFast = MultiplesOf10TablePtr + 1;
 
                 var highestPalindrome = GetHighestPalindrome();
                 
@@ -162,10 +162,26 @@ namespace NextPalindrome
                     }
                 }
             }
+
+            if (true)
+            {
+                var multiplesOf10Table = MultiplesOf10TablePtr;
+                
+                for (uint i = 0; i < 99_999; i++)
+                {
+                    if (ReverseDigitsFast(i, ReversedTwoDigitsTable, GetDigits(i)) != ReverseDigits(i))
+                    {
+                        throw new Exception($"{nameof(ReverseDigitsFast)} failed: {i}");
+                    }
+                }
+            }
+            
             
             for (uint i= 0; i < 1_000_000; i++)
             {
+                #if PRINT_DEBUG
                 Console.WriteLine($"Current Term: {i}");
+                #endif
                 
                 if (GetCurrentOrNextPalindrome(i) != NextPalindromeNaive(i))
                 {
@@ -268,21 +284,193 @@ namespace NextPalindrome
                     continue;
                 }
 
-                break;
+                return newNum;
             }
-
-            return newNum;
         }
 
-        private static ReadOnlySpan<uint> MultiplesOf10 => new uint[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
+        // private static uint[] GenerateReverseTwoDigitsTable()
+        // {
+        //     // Yes, we don't have to handle numbers < 10, but we still allocate extra anyway.
+        //     // This will save us from decrementing by 11 every loop.
+        //     // We also generate reverse variant for 0 - 9 ( Which will return the same number anyway ) to simplify
+        //     // loop in ReverseDigitsFast(), allowing to do newNum += reversedTwoDigitsTable[rem] even when rem <= 9.
+        //     const uint START = 0, END = 99;
+        //     
+        //     // + 1, as array indexes are 0-based.
+        //     // E.x. An index of 99 would mean accessing the 100 th element in the array.
+        //     const int LENGTH = (int) (END + 1);
+        //
+        //     var arr = GC.AllocateUninitializedArray<uint>(LENGTH, true);
+        //     
+        //     var num = START;
+        //     
+        //     for (; num <= END; num++)
+        //     {
+        //         arr[num] = ReverseDigits(num);
+        //     }
+        //     
+        //     return arr;
+        // }
+        //
+        // private static readonly uint[] ReversedTwoDigitsTable = GenerateReverseTwoDigitsTable();
+        
+        // Yes, we don't have to handle numbers < 10, but we still allocate extra anyway.
+        // This will save us from decrementing by 11 every loop.
+        // We also generate reverse variant for 0 - 9 ( Which will return the same number anyway ) to simplify
+        // loop in ReverseDigitsFast(), allowing to do newNum += reversedTwoDigitsTable[rem] even when rem <= 9.
+        private const uint REVERSE_TWO_DIGITS_TABLE_START = 0, 
+                           REVERSE_TWO_DIGITS_TABLE_END = 99;
+        
+        // + 1, as array indexes are 0-based.
+        // E.x. An index of 99 would mean accessing the 100 th element in the array.
+        private const int REVERSE_TWO_DIGITS_TABLE_LENGTH = (int) (REVERSE_TWO_DIGITS_TABLE_END + 1);
+        
+        private static sbyte* GenerateReverseTwoDigitsTable()
+        {
+            var table = (sbyte*) NativeMemory.AlignedAlloc(REVERSE_TWO_DIGITS_TABLE_LENGTH * sizeof(sbyte), 64);
+            
+            var num = REVERSE_TWO_DIGITS_TABLE_START;
+            
+            // Could be more efficient, but who cares
+            for (; num <= REVERSE_TWO_DIGITS_TABLE_END; num++)
+            {
+                var reversed = (num < 10) ? (int) (num * 10) : (int) ReverseDigits(num);
+             
+                // var endsWithZero = num % 10 == 0;
+                //
+                // if (endsWithZero)
+                // {
+                //     reversed = -reversed;
+                // }
+                
+                table[num] = (sbyte) reversed;
+            }
+            
+            return table;
+        }
+        
+        private static readonly sbyte* ReversedTwoDigitsTable = GenerateReverseTwoDigitsTable();
+        
+        // This is mainly for debugging, don't actually use this.
+        public static ReadOnlySpan<sbyte> ReversedTwoDigitsTableSpan => new ReadOnlySpan<sbyte>(ReversedTwoDigitsTable, REVERSE_TWO_DIGITS_TABLE_LENGTH);
+        
+        // [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        // private static uint ReverseDigitsFast(uint num, sbyte* reversedTwoDigitsTable)
+        // {
+        //     uint newNum = 0;
+        //
+        //     while (true)
+        //     {
+        //         var cont = num > 99;
+        //         
+        //         (num, var rem) = DivRemFast(num, 100);
+        //         
+        //         // Reverse remainder and insert. Do so even if num <= 9 ( See comment in GenerateReverseTwoDigitsTable() )
+        //         
+        //         // 120
+        //         // 20 ( First pair )
+        //         // endsWithZero: True, cont: True
+        //         // 20 -> 2 ( Reverse )
+        //         // 2 + sum ( 0 ) = 2 ( New sum )
+        //         // sum * 10 = 20 ( New sum ) [ Happens because cont == true ]
+        //         // 1 ( Second pair )
+        //         // endsWithZero: True, cont: False
+        //         // 1 -> 1 ( Reverse )
+        //         // 1 + sum ( 20 ) = 21 ( New sum )
+        //         // Multiplication is skipped, since cont == false
+        //         
+        //         var tableValue = reversedTwoDigitsTable[rem];
+        //
+        //         // Ironic, I know. But we do not consider 0 to endWithZero, as 0 multiplied by anything is well...itself
+        //         var endsWithZero = tableValue < 0;
+        //
+        //         var addedValue = endsWithZero ? (uint) -tableValue : (uint) tableValue;
+        //         
+        //         newNum += addedValue;
+        //
+        //         if (cont)
+        //         {
+        //             var multiplier = (endsWithZero || num < 10) ? (uint) 10 : 100;
+        //             newNum *= multiplier;
+        //             continue;
+        //         }
+        //
+        //         return newNum;
+        //     }
+        // }
+        
+        [SkipLocalsInit]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint ReverseDigitsFast(uint num, sbyte* reversedTwoDigitsTable, int digitCount)
+        {
+            //uint newNum = 0;
 
-        private static readonly uint* MultiplesTable = (uint*) Unsafe.AsPointer(ref MemoryMarshal.GetReference(MultiplesOf10));
+            var isOdd = true;
+            
+            Unsafe.SkipInit(out uint block1);
+            Unsafe.SkipInit(out uint block2);
+            Unsafe.SkipInit(out uint block3);
+            Unsafe.SkipInit(out uint block2Multiplier);
+            
+            switch (digitCount)
+            {
+                case 5:
+                    goto DivTwice;
+                case 4:
+                    isOdd = false;
+                    goto DivTwice;
+                case 3:
+                    goto DivOnce;
+                case 2:
+                    return (uint) reversedTwoDigitsTable[num];
+                default: // case 1:
+                    goto One;
+            }
+            
+            DivTwice:
+            (num, block1) = DivRemFast(num, 100);
+            block1 = (uint) reversedTwoDigitsTable[block1];
+            DivOnce:
+            (num, block2) = DivRemFast(num, 100);
+            // var block2GreaterEqual10 = block2 >= 10;
+            block2 = (uint) reversedTwoDigitsTable[block2];
+            
+            if (!isOdd)
+            {
+                goto Ret;
+            }
+
+            // block2Multiplier = block2GreaterEqual10 ? (uint) 0_00_10 : 0_01_00;
+            
+            ProcessRemainder:
+            block3 = num; // (uint) reversedTwoDigitsTable[num];
+            
+            Ret:
+            switch (digitCount)
+            {
+                case 5:
+                    return (block1 * 0_10_00) + (block2 * 0_00_10) + block3;
+                case 4:
+                    return (block1 * 0_01_00) + block2;
+                case 3:
+                    return (block2 * 0_00_10) + block3;
+                // case 2:
+                //     return block3;
+            }
+            
+            One:
+            return num;
+        }
+
+        private static ReadOnlySpan<uint> MultiplesOf10Table => new uint[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
+
+        private static readonly uint* MultiplesOf10TablePtr = (uint*) Unsafe.AsPointer(ref MemoryMarshal.GetReference(MultiplesOf10Table));
         
         // [MethodImpl(MethodImplOptions.NoInlining)] // For checking codegen
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static uint GetCurrentOrNextPalindrome(uint num)
         {
-            var multiplesTable = MultiplesTable;
+            var multiplesTable = MultiplesOf10TablePtr;
             
             // This should be constant-folded
             var multipleTableGetDigitsFast = multiplesTable + 1;
@@ -339,8 +527,11 @@ namespace NextPalindrome
             #if PRINT_DEBUG
             Console.WriteLine($"{left}{right}");
             #endif
+
+            var reversedTwoDigitsTable = ReversedTwoDigitsTable;
             
-            var leftReversed = ReverseDigits(left);   
+            // var leftReversed = ReverseDigits(left);   
+            var leftReversed = ReverseDigitsFast(left, reversedTwoDigitsTable, digitsPerHalf);
 
             #if PRINT_DEBUG
             Console.WriteLine($"R: {right} | L-R: {leftReversed}");
@@ -383,7 +574,8 @@ namespace NextPalindrome
             Console.WriteLine($"Unfortunate: {leftMiddleTermInclusive}(L+M)");
             #endif
             
-            leftReversed = ReverseDigits(left);
+            // leftReversed = ReverseDigits(left);
+            leftReversed = ReverseDigitsFast(left, reversedTwoDigitsTable, digitsPerHalf);
             #if PRINT_DEBUG
             Console.WriteLine($"Unfortunate: L-R ( Post ): {leftReversed}");
             #endif
